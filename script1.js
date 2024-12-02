b7f4d2d0325402310b0ae8d618eab0bb9254f3df
Ashkan <vafaeiashkan81@gmail.com>
2024-12-02 08:50:07 -0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -3,9 +3,6 @@ const todoInput = document.getElementById('todo-input');
 const addButton = document.getElementById('add-button');
 const todoList = document.getElementById('todo-list');
 const priorScale = document.getElementsByName('prioritize'); // this returns a collection of elements with the name "prioritize"
-console.log(priorScale);
-const summary = document.getElementsByClassName('summaryBox');
-var numTask = 0;
 
 function changeTheme ( theme ) {
   changeBackgroundColor(theme);
@@ -13,14 +10,12 @@ function changeTheme ( theme ) {
 }
 
 function changeBackgroundColor( theme ) {
-   if (theme === 'study') {  
+  if (theme === 'study') {  
     document.body.style.backgroundColor = "lightblue"; 
     document.body.style.backgroundImage = "url('images/studyImage2.jpg')";
-
   } else if (theme === 'vacation') {
     document.body.style.backgroundColor = "lightyellow";
     document.body.style.backgroundImage = "url('images/vacationImage2.jpg')";
-
   } else if (theme === 'finance') {
     document.body.style.backgroundColor = "lightgreen";
     document.body.style.backgroundImage = "url('images/financeImage.jpg')";
@@ -39,123 +34,85 @@ function changeBackgroundColor( theme ) {
   }
 }
 
-function changeTitle(theme) {
-  switch (theme) {
-    case "study":
-      document.querySelector("h1").textContent = "To-do List for Study";
-      break;
-    case "vacation":
-      document.querySelector("h1").textContent = "To-do List for Vacation";
-      break;
-    case "finance":
-      document.querySelector("h1").textContent = "To-do List for Finance";
-      break;
-    case "work":
-      document.querySelector("h1").textContent = "To-do List for Work";
-      break;
-    case "chores":
-      document.querySelector("h1").textContent = "To-do List for Housework";
-      break;
-    case "grocery":
-      document.querySelector("h1").textContent = "To-do List for Grocery Shopping";
-      break;
-  }
-}
-
-
 function addTask() {
   const taskText = todoInput.value.trim();
-  if (taskText) { // if there is text entered, then add the task to the list
-    const listItem = document.createElement('li'); 
-    const importScale = document.createElement('p');
-    
-    // console.log(importScale);
-
-    // Put the task entered as a new item in a list 
-    listItem.textContent = taskText;
-    
-    // Declare a new dictionary
-    var dictTask = {}; 
-
-    // if one of the scale is checked, then add it to the task corresponding to it
-    // then uncheck it
-    // if the scale isn't checked, then don't add anything
-    if (priorScale[0].checked) {
-      
-      dictTask[taskText] = priorScale[0].value;
-      importScale.textContent = priorScale[0].id;
-      // uncheck it
-      priorScale[0].checked = false; 
-
-    } else if (priorScale[1].checked) {
-      // listItem.textContent += " " + priorScale[1].value;
-      dictTask[taskText] = priorScale[1].value;
-      importScale.textContent = priorScale[1].id;
-      // uncheck it
-      priorScale[1].checked = false;
-
-    } else if (priorScale[2].checked) {
-      // listItem.textContent += " " + priorScale[2].value;
-      dictTask[taskText] = priorScale[2].value;
-      importScale.textContent = priorScale[2].id;
-      // uncheck it
-      priorScale[2].checked = false;
-    } else {
-      dictTask[taskText] = "0";
-    }
-
-    // Add the <p> tag for each task
-    listItem.appendChild(importScale);
-    
-    
-    // For every new item/task, create a button 
-    const removeButton = document.createElement('button');
-    // Make the value of the button 'Remove
-    removeButton.textContent = 'Remove';
-    // When click on the remove button, pass it on to the function
-    removeButton.addEventListener('click', removeTask);
-    // removeButton.onclick = () => listItem.remove();
-    
-    // Add the remove button to each task
-    listItem.appendChild(removeButton);
-    // Add each task to the end of the list of child of todoList
-    todoList.appendChild(listItem);
-    // Clear out the input bar'
-    todoInput.value = '';
-
-    /* Count the number of task. 
-    Add one every time a new task is made
-    Then display the count to HTML*/
-    numTask += 1;
-    document.getElementById("numSum").innerText = numTask;
+  if (!taskText) {
+    alert("Please enter a task!");
+    return;
   }
+
+  const listItem = document.createElement('li');
+  listItem.textContent = taskText;
+
+  // Check for priority selection and append it to the task
+  for (let i = 0; i < priorScale.length; i++) {
+    if (priorScale[i].checked) {
+      listItem.textContent += ` (${priorScale[i].value})`;
+      priorScale[i].checked = false; // Uncheck after using
+      break;
+    }
+  }
+
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove';
+  removeButton.onclick = () => {
+    if (confirm("Are you sure that you want to remove this task?")) {
+      listItem.remove();
+    }
+  };
+  removeButton.onclick = () => {
+    const confirmationBox = document.createElement('div');
+    confirmationBox.style.position = 'fixed';
+    confirmationBox.style.top = '50%';
+    confirmationBox.style.left = '50%';
+    confirmationBox.style.transform = 'translate(-50%, -50%)';
+    confirmationBox.style.padding = '20px';
+    confirmationBox.style.backgroundColor = 'white';
+    confirmationBox.style.border = '1px solid black';
+    confirmationBox.style.zIndex = '1000';
+
+    const confirmationText = document.createElement('p');
+    confirmationText.textContent = 'Are you sure that you want to remove this task?';
+    confirmationBox.appendChild(confirmationText);
+
+    const yesButton = document.createElement('button');
+    yesButton.textContent = 'Yes';
+    yesButton.style.backgroundColor = 'green';
+    yesButton.style.color = 'white';
+    yesButton.style.borderRadius = '5px';
+
+    yesButton.onclick = () => {
+      listItem.remove();
+      document.body.removeChild(confirmationBox);
+    };
+    confirmationBox.appendChild(yesButton);
+
+    const noButton = document.createElement('button');
+    noButton.textContent = 'No';
+    noButton.style.backgroundColor = 'green';
+    noButton.style.color = 'white';
+    noButton.style.float = 'right';
+    noButton.style.borderRadius = '5px';
+    noButton.onclick = () => {
+      document.body.removeChild(confirmationBox);
+    };
+    confirmationBox.appendChild(noButton);
+
+    document.body.appendChild(confirmationBox);
+  };
+  
+  //removeButton.onclick = () => listItem.remove();
+
+  listItem.appendChild(removeButton);
+  todoList.appendChild(listItem);
+  todoInput.value = '';
 }
-
-function removeTask(event) {
-  /* .currentTarget returns the element that triggers the event.
-  In this case, it is the removeButton */
-  var elmt = event.currentTarget;
-  // get the parent element of the button, which is listItem
-  var parEle = elmt.parentElement;
-  // then remove the listItem
-  parEle.remove();
-
-  /* Update the number of task.
-  Subtract one everytime the task is deleted
-  Then display the count to HTML */
-  numTask -= 1;
-  document.getElementById("numSum").innerText = numTask;
-}
-
-function sortTask() {
-  // Sort based on the value of the dictionary key
-}
-
-// Add respond event when click on add button
 addButton.addEventListener('click', addTask);
-// Also respond when press enter
+
+
+
 todoInput.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     addTask();
   }
-});
+}); 
